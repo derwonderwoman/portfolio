c50bdc8a3a01bc7a7cddb426d5e320b30d3c81f6
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-18 15:16:00 +0300

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,27 @@ const Projects = () => {
     return (
         <>
         <NavBarForProj/>
+        <h1 className='title'>My projects</h1>
+        <div className='project-container'>
+            <a href="https://thelocals-fe.onrender.com/" className="Bigcircle">
+                <div>
+                theLocals
+                <p>Web Service for Connecting Clients with Local Service Providers</p>
+                </div>
+                </a>
+                <a href="" className="Bigcircle">
+                <div>
+                MordecaiHatesHamam
+                <p>A Telegram bot dedicated to finding and combating antisemitic comments on YouTube.</p>
+                </div>
+                </a>
+                <a href="" className="Bigcircle">
+                <div>
+                MoneyManager
+                <p>An intuitive web service designed to track and manage expenses efficiently</p>
+                </div>
+                </a>
+        </div>  
         <Footer/>
         </>
     );
