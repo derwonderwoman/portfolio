923ce0024e40e76d977fadfff599f6315002b049
derwonderwoman <tokarevakseniia@gmail.com>
2024-06-19 12:10:15 +0300

    before responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Footer from './Footer';
 import NavBarAbout from './NavBars/NavBarAbout';
 import '../Carousel.css';
@@ -10,40 +10,35 @@ const AboutMe = () => {
     <>
       <NavBarAbout />
       <div className="improvedContent">
-        <h1 className='SkillTitle'>About Me</h1>
-        <p>Originally from Russia, I moved to Israel five years ago for new challenges and opportunities. My academic journey began with a focus on Russian Area Studies, culminating in a Bachelor's degree from RANEPA in Moscow. However, my career path took a significant turn when I discovered my passion for programming.
-          I am eager to leverage my skills and passion for innovation in Israel's dynamic tech industry.</p>
-        <h1 className='SkillTitle'>Skills</h1>
+        <div className="centered-button">
+          <a className='downloadCVAbout' href="/Kseniia Tokarev CV .pdf" download="Kseniia Tokareva CV.pdf">My CV</a>
+        </div>
+        <h1 className='SkillTitle'>Small Facts About Me</h1>
+        <div className="about-container">
+          <div className="fact">I moved to Israel five years ago from Russia. </div>
+          <div className="fact">Fluent in Russian, English, Hebrew; basic French skills</div>
+          <div className="fact">I was honored to contribute my project to the Adir Antisemitic initiative</div>
+          <div className="fact">Leadership role in winning a <strong>Hackathon</strong> with innovative projects.</div>
+          <div className="fact">Prior experience in organizational roles at international firms.</div>
+          <div className="fact">Achieved success at TLV Coding Bootcamp in web development.</div>
+        </div>
+        <h1 className='SkillTitle'>Skills.</h1>
         <div className="skills-container">
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" />
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg" />
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" />
-            
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/redux/redux-original.svg" />
-            
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" />
-            
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
-            
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-          
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
-          
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
-          
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
-          
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" />
-          
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" />
-          
-            <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" />
-          
-
-          
+            <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" /> 
+            <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" /> 
         </div>
-        <h1 className='SkillTitle'>Education</h1>
-        <p>Now a dedicated Full Stack Developer specializing in Python and JavaScript, with a solid foundation in C programming, I underwent intensive training at Infinity Labs, TLV Coding Bootcamp in Ramat-Gan, Israel. My focus shifted from embedded programming to web development, leading me to excel at the Developers Institute, TLV Coding Bootcamp. Among my achievements is winning a Hackathon, where I showcased my creativity and technical proficiency in Python, JavaScript, and other key technologies crucial for Full Stack Web Development. An exemplary project from my portfolio is "TheLocals - local service tracker," a robust web application built with React.js, TypeScript, and Node.js with Express, featuring JWT authentication for enhanced security.</p>
       </div>
       <Footer />
     </>
